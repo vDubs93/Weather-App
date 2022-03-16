14c34cf09a74b6d0b21df0063ebc824cbe1db540
William <william.vicic@gmail.com>
2022-03-15 19:18:17 -0500

    -removed unused variables and code from CurrentViewModel.kt

--- app/src/main/java/com/example/weatherstation/CurrentViewModel.kt
+++ app/src/main/java/com/example/weatherstation/CurrentViewModel.kt
@@ -1,13 +1,6 @@
 package com.example.weatherstation
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import javax.inject.Inject
 
-class CurrentViewModel @Inject constructor(private val service: Api) : ViewModel() {
-
-    val currentConditions: MutableLiveData<CurrentConditions> = MutableLiveData()
-
+class CurrentViewModel : ViewModel() {
 }
