cd85a1d17cf0fe364b8847842bb034aae83ad6f5
William Vicic <william.vicic@gmail.com>
2022-03-23 16:43:23 -0500

    Re-added @Inject to CurrentViewmodel constructor because not having it there broke the app

--- app/src/main/java/com/example/weatherstation/CurrentViewModel.kt
+++ app/src/main/java/com/example/weatherstation/CurrentViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.weatherstation
 
 import androidx.lifecycle.ViewModel
+import javax.inject.Inject
 
-class CurrentViewModel : ViewModel() {
+class CurrentViewModel @Inject constructor(private val service : Api): ViewModel() {
 }
